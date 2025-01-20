8116696ac9b7164fc370fc5064d0f0e1a929dfba
sensh11 <alex.tursumetov@mail.ru>
2025-01-20 22:24:50 +0600

    feat : changed bugs from previous version

--- db.js
+++ db.js
@@ -13,14 +13,14 @@ module.exports = fmsdb;
 //mongo db connect snippet
 const { MongoClient } = require('mongodb');
 
-const uri = "your_mongodb_atlas_connection_string"; // Replace with your MongoDB Atlas connection string
+const uri = "mongodb+srv://sanek001:12345678987654321@sensh1.l0lry.mongodb.net/?retryWrites=true&w=majority&appName=Sensh1";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToDatabase() {
   try {
     await client.connect();
     console.log("Connected to MongoDB Atlas");
-    const database = client.db('fmsdb'); // Replace 'fmsdb' with your database name
+    const database = client.db('users'); // Replace 'fmsdb' with your database name
     return database;
   } catch (error) {
     console.error("Error connecting to MongoDB Atlas", error);
