d72c7749ea1f2943e43b92f3f95d7d7bbc4b5171
sensh11 <alex.tursumetov@mail.ru>
2025-01-20 21:18:54 +0600

    feat :added connection to MongoDB

--- db.js
+++ db.js
@@ -10,3 +10,22 @@ const fmsdb = new Pool({
 
 module.exports = fmsdb;
 
+//mongo db connect snippet
+const { MongoClient } = require('mongodb');
+
+const uri = "your_mongodb_atlas_connection_string"; // Replace with your MongoDB Atlas connection string
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB Atlas");
+    const database = client.db('fmsdb'); // Replace 'fmsdb' with your database name
+    return database;
+  } catch (error) {
+    console.error("Error connecting to MongoDB Atlas", error);
+    throw error;
+  }
+}
+
+module.exports = connectToDatabase;
