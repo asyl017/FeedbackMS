38a39220cce96239556388d1ebef6230d4aa2bcc
yerassylv <bolatkannn@gmail.com>
2025-02-24 19:55:27 +0600

    feat:Added dockerfile

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    otp: { type: String }, // Поле для хранения OTP
-    verified: { type: Boolean, default: false } // Поле для статуса верификации
+    otp: { type: String },
+    verified: { type: Boolean, default: false } 
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const feedbackController = require('../controllers/feedbackController');
 
-// Проверяем наличие функций контроллера
+
 console.log('Controller functions:', feedbackController);
 
-// Определяем маршруты
 router.get('/feedbacks', (req, res) => {
     feedbackController.getAllFeedbacks(req, res);
 });
